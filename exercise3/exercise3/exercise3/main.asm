fce6c8c0ac2755b1ecf4a706d6ff24c01cbb3a72
Radu Orleanu <radu.orleanu@gmail.com>
2018-03-08 13:54:03 +0100

    solved exercise 3

---
+++ exercise3/exercise3/exercise3/main.asm
@@ -0,0 +1,11 @@
+;
+; exercise3.asm
+;
+; Created: 3/8/2018 1:31:18 PM
+; Author : Radu
+;
+
+ldi r16, 42			; loading value into register 16
+ldi r17, 0x33		; loading value into register 17
+ldi r18, 0b10110010	; loading positive binary value 10110010 			
+nop					; nop operation takes 1 clock cycle but does absolutely nothing
