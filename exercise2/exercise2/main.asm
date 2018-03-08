0399542e0d11c3665b333438f6b92925b8959d15
Radu Orleanu <radu.orleanu@gmail.com>
2018-03-08 11:47:38 +0100

    example commit

---
+++ exercise2/exercise2/main.asm
@@ -0,0 +1,10 @@
+;
+; exercise2.asm
+;
+; Created: 3/8/2018 11:45:35 AM
+; Author : Radu
+;
+
+
+ldi r16, 0b0100_0010    ; load value into register r16
+nop                     ; does nothing, but provides a place to put a breakpoint
