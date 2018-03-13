c614942173436855bf63ee8768732fc41921e162
Radu Orleanu <radu.orleanu@gmail.com>
2018-03-13 11:54:26 +0100

    completed exercise 5 Hello World (or turn on a led)

--- HelloWorld/HelloWorld/main.asm
+++ HelloWorld/HelloWorld/main.asm
@@ -4,11 +4,11 @@
 ; Created: 3/7/2018 9:21:47 PM
 ; Author : chfs
 ;
-
+	; ddrc
                             ; SETUP
     ldi r16, 0xff           ; load the bit pattern 1111 1111 into r16
     out ddra, r16           ; write the bit pattern to port a's data direction register
 
                             ; MAIN
-    ldi r16, ~(1<<pina)     ; invert the bit pattern 0000 0001 and write it to r16
+    ldi r16, ~(1<<pa0)      ; invert the bit pattern 0000 0001, and write it to r16
     out porta, r16          ; write the pattern to port a's output control register
--- exercise2/exercise2/main.asm
+++ exercise2/exercise2/main.asm
@@ -6,5 +6,5 @@
 ;
 
 ldi r16, 0b0100_0010    ; load value into register r16
-nop                     ; does nothing, but provides a place to put a breakpoint
+nop                      ; does nothing, but provides a place to put a breakpoint
 	
